0bf31b51649886244d97b73e11b0c2919a1f68e6
prafpawar11 <prafpawar11@gmail.com>
2022-06-06 20:11:02 +0530

    third commit

---
+++ src/Tutorial1/Demo3.java
@@ -0,0 +1,11 @@
+package Tutorial1;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Demo3 cclass is addeds");
+	}
+
+}
